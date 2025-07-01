eeb320d0da5197fb1a6cbc68b4a890fb9c697c51
Prajwal Ghadge <prajwalghadge2005@gmail.com>
2025-07-01 17:05:32 +0530

    updating sandobx to work per session 2

--- python-backend/assistant.py
+++ python-backend/assistant.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from textwrap import dedent
 import logging
 
-from typing import Optional, List, Any
+from typing import Optional, List, Any, Set
 
 from agno.agent import Agent, AgentSession
 from agno.utils.log import log_debug
